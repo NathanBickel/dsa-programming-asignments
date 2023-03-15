63658ad5dd7193818d58b901b08c1d95a79636f8
NathanBickel <nbickel@email.sc.edu>
2023-03-15 12:39:13 -0400

    Assignment 1 Problem 1

---
+++ assignment1/problem1/README.md
@@ -0,0 +1,21 @@
+Preconditions: 
+* "input.txt" exists in the current directory
+* File contains n rows, where n is some integer > 3
+* Each row contains n space-seperated strings, where each
+string is exactly "true" or "false"
+* The boolean matrix is the adjacency matrix for a simple graph
+on n vertices
+
+Postconditions:
+* Solution.java will generate a file named "output.txt"
+* "output.txt" will contain "ring" if the graph is a cycle,
+"star" if the graph is a star, "mesh" if the graph is a
+complete graph, and "none" otherwise.
+* "output.txt" will also contain the execution time for
+running the algorithm, excluding time of input/output.
+This time will be in nanoseconds.
+
+To run:
+1. Create a file "input.txt" in the current directory
+2. javac Solution.java
+3. java Solution.java
---
+++ assignment1/problem1/Solution.java
@@ -0,0 +1,154 @@
+import java.io.*;
+import java.util.Scanner;
+
+import java.util.ArrayList;
+
+public class Solution {
+
+    public static int n;
+    public static boolean[][] matrix;
+
+    public static void main(String[] args) {
+        populateMatrix("input.txt");
+        long startTime = System.nanoTime();
+        String topology = determineTopology();
+        long endTime = System.nanoTime();
+        outputTopology("output.txt", topology 
+            + "\n" + (int)(endTime - startTime) + " nanoseconds");
+    }
+
+    private static void populateMatrix(String fileName) {
+        try {
+            Scanner fileReader = new Scanner(new File(fileName)); // Try to open file
+            // Determine n
+            n = 0;
+            while (fileReader.hasNextLine()) {
+                n++;
+                fileReader.nextLine();
+            }
+            matrix = new boolean[n][n]; // Initialize matrix
+            fileReader = new Scanner(new File(fileName)); // Open file again
+            for (int r = 0; r < n; r++)
+                for (int c = 0; c < n; c++)
+                    matrix[r][c] = fileReader.nextBoolean(); // Populate matrix
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean checkRing() {
+        ArrayList<Integer> visited = new ArrayList<Integer>();
+        visited.add(0);
+        int next = -1; // Should go forward through this vertex
+        int end = -1; // Should return back to 0 through this vertex
+        for (int c = 1; c < n; c++) {
+            if (matrix[0][c]) {
+                if (next == -1) {
+                    next = c;
+                } else if (end == -1) {
+                    end = c;
+                } else {
+                    // Too many connections
+                    return false;
+                }
+            }
+        }
+        if (next == -1 || end == -1) {
+            // We didn't find a suitable start and end
+            return false;
+        }
+        visited.add(next);
+        while (true) {
+            boolean foundNext = false;
+            boolean foundPrev = false;
+            int current = visited.get(visited.size() - 1);
+            int prev = visited.get(visited.size() - 2);
+            next = -1;
+            for (int c = 0; c < n; c++) {
+                if (matrix[current][c]) {
+                    if (c == prev) {
+                        // Check if this is where we came from
+                        foundPrev = true;
+                    } else if (!foundNext) {
+                        next = c;
+                        foundNext = true;
+                    } else {
+                        // Too many connections
+                        return false;   
+                    }
+                }
+            }
+            if (!foundNext || !foundPrev) {
+                // Didn't find somewhere to go, or somewhere to come from
+                return false;
+            } else if (next == end) {
+                // We made it to the end!
+                return true;
+            } else if (visited.contains(next)) {
+                // We've already been here, and we're not back to the start
+                return false;
+            } else {
+                // We didn't run into any problems, so let's keep going
+                visited.add(next);
+            }   
+        }
+    }
+
+    private static boolean checkStar() {
+        int center = -1; // Need to find vertex conected to everything else
+        if (matrix[0][1] && matrix[0][2])
+            // There are two edges connected to 0, so 0 must be center
+            center = 0;
+        else
+            // There is an edge not connected to 0 (1 or 2), so 0 isn't center
+            for (int c = 1; c < n; c++)
+                if (matrix[0][c])
+                    // We found the center
+                    center = c;
+        if (center == -1)
+            // No center, so can't be a star
+            return false;
+        // Now, check that edge uv is present iff u xor v is the center 
+        for (int r = 0; r < n - 1; r++) {
+            for (int c = r + 1; c < n; c++) {
+                if ((r == center || c == center) && !matrix[r][c])
+                    // Missing edge from center
+                    return false;
+                else if (r != center && c != center && matrix[r][c])
+                    // Edge not adjancent to center is present
+                    return false;
+            }
+        }
+        return true; // Everything checks out
+    }
+
+    private static boolean checkMesh() {
+        for (int r = 0; r < n - 1; r++)
+            for (int c = r + 1; c < n; c++)
+                if (!matrix[r][c])
+                    return false; // Not complete: edge rc is not present
+        return true;
+    }
+
+    private static String determineTopology() {
+        if (checkRing())
+            return "ring";
+        else if (checkStar())
+            return "star";
+        else if (checkMesh())
+            return "mesh";
+        else
+            return "none";
+    }
+
+    private static void outputTopology(String fileName, String topology) {
+        try {
+            PrintWriter fileWriter = new PrintWriter(new FileOutputStream(new File(fileName)));
+            fileWriter.println(topology);
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
